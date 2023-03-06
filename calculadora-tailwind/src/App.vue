aaf080c4a6d83af27cf7fbe673143ab9d881674b
jhulyamfebr@gmail.com <jhulyambrozini@gmail.com>
2023-03-06 15:30:10 -0300

    chore: configurando projeto

---
+++ calculadora-tailwind/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+  import Calculadora from './components/Calculadora.vue'
+</script>
+
+<template>
+  <Calculadora />
+</template>
---
+++ calculadora-tailwind/src/components/Calculadora.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="bg-violet-500">
+
+        <h1>oi</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
